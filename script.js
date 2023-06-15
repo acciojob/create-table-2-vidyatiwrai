e22f9c4afa57e7ecd82ae167135ed07751fa2587
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-06-15 08:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+
+
 function createTable() {
-    //Write your code here
-  
+  const rowCount = prompt('Input number of rows');
+  const columnCount = prompt('Input number of columns');
+
+  const table = document.getElementById('myTable');
+
+  // Clear existing table content
+  table.innerHTML = '';
+
+  // Create rows and cells
+  for (let i = 0; i < rowCount; i++) {
+    const row = table.insertRow();
+    for (let j = 0; j < columnCount; j++) {
+      const cell = row.insertCell();
+      cell.textContent = `Row-${i} Column-${j}`;
+    }
+  }
 }
+
+  
+
